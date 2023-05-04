c45df68dbf2db8f026f20946cc5a75711f43262f
David <dtovmasyan2022@gmail.com>
2023-05-04 20:02:18 +0200

    title

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,56 +1,77 @@
 <script setup>
-
 import Form from './Form.vue';
-function myAlert() {
-    alert("message");
-}
-
 </script>
 
 <template>
-    <div class="page_container">
-        <header @click="myAlert">
-          <img src="/logo.png" alt="Logo" class="logo">
-          
-        </header>
-        <main>
-          <div class="list">
-            <div className="searchBar">
-                <input type="search" placeholder="Search" class="Search">
-            </div>
-          </div>
-        </main>
-          
-        <footer>Footer</footer>
-    </div> 
-    <Form></Form> 
+  <div class="page_container">
+    <header>
+      <img src="/logo.png" alt="Logo" class="logo">
+
+    </header>
+    <main>
+
+      <div class="contacts">
+        <div class="searchBar">
+          <input type="search" placeholder="Search" class="Search">
+        </div>
+      </div>
+      <div class="line"></div>
+      <div class="chat">
+        <div class="info">
+          <div class="nickname"></div>
+          <div class="settings"></div>
+        </div>
+      </div>
+    </main>
+
+
+  </div>
+  <Form></Form>
 </template>
 
 <style scoped>
-
-.page_container{
+.page_container {
   display: grid;
-  grid-template-rows: 100px calc(100vh - 100px - 40px) 40px;
+  grid-template-rows: 100px calc(100vh - 100px);
 }
-header{
+
+header {
   height: 100px;
   display: flex;
   justify-content: center;
-  background-image: linear-gradient(to right,rgb(4, 105, 42), rgb(58, 245, 51));
+  background-image: linear-gradient(to right, rgb(46, 147, 85), rgb(58, 245, 51), rgb(46, 147, 85));
 }
-.logo{
+
+.logo {
   width: 250px;
-  height:50px;
+  height: 50px;
   margin-top: 20px;
-  
+
+}
+
+main {
+  display: grid;
+  grid-template-columns: 20% 80%;
+  grid-template-rows: 100%;
+}
+
+.contacts {
+  display: flex;
+  justify-content: center;
 }
 
 .Search {
-    background-color: rgb(196, 196, 196);
-    border-radius: 10px;
-    border: 1px solid black;
-    max-width: 18em;
+  background-color: rgb(2, 255, 124);
+  border-radius: 10px;
+  border: 1px solid blue;
+  width: 150px;
+  height: 25px;
 }
 
-
+.line {
+  display: inline-block;
+  background-color: rgb(86, 206, 86);
+  width: 5px;
+  height: 100%;
+}
 </style>
